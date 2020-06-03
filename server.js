9183405149673ea30c1a97c745b239c24341446f
Awn Umar <awn@spacetime.dev>
2020-06-03 21:21:45 +0100

    ...

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const port = 4444;
 
 // everything else served from public/
-app.use(express.static("public"));
+app.use(express.static("ephemeral-webapp/dist"));
 
 // start listening
 app.listen(port);
