d69bd756f376d3d67b5e338fb469854df3a355b7
Awn Umar <awn@spacetime.dev>
2020-06-08 00:49:38 +0100

    ...

--- web-app/src/components/Chat.vue
+++ web-app/src/components/Chat.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="content">
     <nav><router-link to="/">Home</router-link></nav>
-    <p>This room's key is <span class="hl">{{ b2h(this.$parent.key) }}</span></p>
+    <p>This room's key is <span class="hl">{{ b2h(this.key) }}</span></p>
     <div id="chat">
-      <div id="history"></div>
+      <div id="history-div">
+        <ul id="history-list">
+          <li v-for="message in messages" v-bind:key="message.id">{{ message.time}} :: <span class="name">{{ message.author }}</span>: {{ message.text }}</li>
+        </ul>
+      </div>
       <div id="input">
-        <form>
+        <form v-on:submit.prevent="sendMessage">
           <input type="text" id="message" autocomplete="off" />
-          <input type="submit" value="send" />
+          <input id="send" type="submit" value="send" />
         </form>
       </div>
     </div>
@@ -15,32 +19,46 @@
 </template>
 
 <script>
+import * as nacl from 'tweetnacl';
 export default {
   name: "Chat",
   data() {
     return {
       key: [],
-      id: "",
-      history: [],
+      roomID: "",
+      messages: [{
+        id: 0,
+        time: 0,
+        author: "alice",
+        text: "test"
+      }],
     }
   },
   methods: {
     b2h(b) {
       // https://stackoverflow.com/a/34356351
-      let hex = [], i = 0;
-      for (; i < b.length; i++) {
+      let hex = [];
+      for (let i = 0; i < b.length; i++) {
           let current = b[i] < 0 ? b[i] + 256 : b[i];
           hex.push((current >>> 4).toString(16));
           hex.push((current & 0xf).toString(16));
       }
       return hex.join("");
+    },
+    sendMessage() {
+      // handle message
     }
   },
   mounted() {
     // If there's no key defined, go to the homepage and ask for one.
     if (this.$parent.key.length == 0) {
       this.$router.push("/");
+      return;
     }
+
+    // Setup local state
+    this.key = this.$parent.key;
+    this.roomID = this.b2h(nacl.hash(this.key).subarray(0, 32));
   }
 };
 </script>
